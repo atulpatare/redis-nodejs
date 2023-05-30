994986982c14fd7c65eaf7badc6a675c7acc3cf8
ap-atul <atulpatare99@gmail.com>
2023-05-30 14:54:57 +0530

    implemented simple caching process

---
+++ README.md
@@ -0,0 +1,4 @@
+### Redis + NodeJS
+
+Trying out redis with nodejs. Simple example to cache a api response.
+
--- server.js
+++ server.js
@@ -1,5 +1,36 @@
 import express from "express";
+import axios from "axios";
+import redis from "redis";
+
+const redisClient = redis.createClient(); // connects to local client
+redisClient.on("error", (error) => console.err(error));
+redisClient.connect();
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+const fetchApiData = async (species) => {
+  const url = `https://www.fishwatch.gov/api/species/${species}`;
+  const apiResponse = await axios.get(url);
+  return apiResponse.data;
+}
+
+app.get("/fish/:species", async (req, res) => {
+  const species = req.params.species;
+  const cacheResults = await redisClient.get(species);
+  if (cacheResults) {
+    res.send({ fromCache: true, data: JSON.parse(cacheResults) });
+    return;
+  }
+  const results = await fetchApiData(species);
+  redisClient.set(species, JSON.stringify(results), {
+    EX: 180,
+    NX: true,
+  });
+  res.send({
+    fromCache: false,
+    data: results,
+  });
+});
 
 app.listen(port, () => console.log("App listening on port ", port));
